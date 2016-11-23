604a6b01bb3fcd242196fcc36ef8bbcf2728b40a
Andhika Srimadeva <andhika.srimadeva@kcl.ac.uk>
2016-11-23 00:37:21 +0000

    Initial API query

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -5,6 +5,12 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.io.*;
+import java.net.URL;
+import java.nio.charset.Charset;
 
 public class Main extends Application {
 
@@ -17,7 +23,36 @@ public class Main extends Application {
     }
 
 
-    public static void main(String[] args) {
-        launch(args);
+    public static void main(String[] args) throws JSONException {
+        //launch(args);
+        JSONArray jsonArray = readJsonFromUrl("http://api.worldbank.org/countries?format=json");
+        JSONArray jsonArray1 = jsonArray.getJSONArray(1);
+        System.out.println(jsonArray1.getJSONObject(0).get("name"));
+
+    }
+
+    public static String readAll(Reader rd) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        int character;
+        while ((character = rd.read()) != -1) {
+            sb.append((char) character);
+        }
+        return sb.toString();
+    }
+
+    public static JSONArray readJsonFromUrl(String url){
+        InputStream is = null;
+        JSONArray json = null;
+        BufferedReader rd = null;
+        try {
+            is = new URL(url).openStream();
+            rd = new BufferedReader(new InputStreamReader(is));
+            String jsonText = readAll(rd);
+            json = new JSONArray(jsonText);
+            is.close();
+        } catch (JSONException | IOException e) {
+            e.printStackTrace();
+        }
+        return json;
     }
 }
