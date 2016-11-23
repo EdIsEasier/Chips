26c83d2ce891f2db50046e66a52b2d8c8647b333
Andhika Srimadeva <andhika.srimadeva@kcl.ac.uk>
2016-11-23 18:26:42 +0000

    Create Indicator class with properties and getters and setters

--- src/data/Country.java
+++ src/data/Country.java
@@ -18,5 +18,100 @@ public class Country{
 
     }
 
+    public String getName() {
+        return name.get();
+    }
+
+    public StringProperty nameProperty() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name.set(name);
+    }
+
+    public String getRegionID() {
+        return regionID.get();
+    }
+
+    public StringProperty regionIDProperty() {
+        return regionID;
+    }
+
+    public void setRegionID(String regionID) {
+        this.regionID.set(regionID);
+    }
+
+    public String getRegionName() {
+        return regionName.get();
+    }
+
+    public StringProperty regionNameProperty() {
+        return regionName;
+    }
+
+    public void setRegionName(String regionName) {
+        this.regionName.set(regionName);
+    }
+
+    public String getIncomeLevel() {
+        return incomeLevel.get();
+    }
+
+    public StringProperty incomeLevelProperty() {
+        return incomeLevel;
+    }
+
+    public void setIncomeLevel(String incomeLevel) {
+        this.incomeLevel.set(incomeLevel);
+    }
+
+    public String getLendingType() {
+        return lendingType.get();
+    }
+
+    public StringProperty lendingTypeProperty() {
+        return lendingType;
+    }
+
+    public void setLendingType(String lendingType) {
+        this.lendingType.set(lendingType);
+    }
+
+    public String getCapitalCity() {
+        return capitalCity.get();
+    }
+
+    public StringProperty capitalCityProperty() {
+        return capitalCity;
+    }
+
+    public void setCapitalCity(String capitalCity) {
+        this.capitalCity.set(capitalCity);
+    }
+
+    public String getLongitude() {
+        return longitude.get();
+    }
+
+    public StringProperty longitudeProperty() {
+        return longitude;
+    }
+
+    public void setLongitude(String longitude) {
+        this.longitude.set(longitude);
+    }
+
+    public String getLatitude() {
+        return latitude.get();
+    }
+
+    public StringProperty latitudeProperty() {
+        return latitude;
+    }
+
+    public void setLatitude(String latitude) {
+        this.latitude.set(latitude);
+    }
 
 }
---
+++ src/data/Indicator.java
@@ -0,0 +1,91 @@
+package data;
+
+
+import javafx.beans.property.StringProperty;
+
+public class Indicator{
+
+    private StringProperty code;
+    private StringProperty name;
+    private StringProperty sourceID;
+    private StringProperty sourceNote;
+    private StringProperty topicID;
+    private StringProperty topicName;
+
+    public Indicator(){}
+
+
+    public String getCode() {
+        return code.get();
+    }
+
+    public StringProperty codeProperty() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code.set(code);
+    }
+
+    public String getName() {
+        return name.get();
+    }
+
+    public StringProperty nameProperty() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name.set(name);
+    }
+
+    public String getSourceID() {
+        return sourceID.get();
+    }
+
+    public StringProperty sourceIDProperty() {
+        return sourceID;
+    }
+
+    public void setSourceID(String sourceID) {
+        this.sourceID.set(sourceID);
+    }
+
+    public String getSourceNote() {
+        return sourceNote.get();
+    }
+
+    public StringProperty sourceNoteProperty() {
+        return sourceNote;
+    }
+
+    public void setSourceNote(String sourceNote) {
+        this.sourceNote.set(sourceNote);
+    }
+
+    public String getTopicID() {
+        return topicID.get();
+    }
+
+    public StringProperty topicIDProperty() {
+        return topicID;
+    }
+
+    public void setTopicID(String topicID) {
+        this.topicID.set(topicID);
+    }
+
+    public String getTopicName() {
+        return topicName.get();
+    }
+
+    public StringProperty topicNameProperty() {
+        return topicName;
+    }
+
+    public void setTopicName(String topicName) {
+        this.topicName.set(topicName);
+    }
+
+
+}
