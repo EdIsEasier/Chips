3ffaf68f932c3978bab9e2e203eca8c4b35acf0b
k1502829 <edvinas.kruglovas@kcl.ac.uk>
2016-12-09 16:46:19 +0000

    Added a splash screen

--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -1,19 +1,99 @@
 package main.java;
 
+import javafx.animation.FadeTransition;
 import javafx.application.Application;
+import javafx.beans.property.ReadOnlyObjectProperty;
+import javafx.concurrent.Task;
+import javafx.concurrent.Worker;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.effect.DropShadow;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.util.Duration;
 
 public class MainApp extends Application{
+    private Pane splashLayout;
+    private ProgressBar loadProgress;
+    private static final int SPLASH_WIDTH = 248;
+    private static final int SPLASH_HEIGHT = 183;
+
+    @Override
+    public void init() {
+        // change to "logo.png" when building with gradle
+        ImageView splash = new ImageView(new Image(getClass().getClassLoader().getResourceAsStream("main/resources/logo.png")));
+        loadProgress = new ProgressBar();
+        loadProgress.setPrefWidth(SPLASH_WIDTH - 20);
+        splashLayout = new VBox();
+        splashLayout.getChildren().addAll(splash, loadProgress);
+        splashLayout.setStyle(
+                "-fx-padding: 5; " +
+                        "-fx-background-color: cornsilk; " +
+                        "-fx-border-width:5; " +
+                        "-fx-border-color: " +
+                        "linear-gradient(" +
+                        "to bottom, " +
+                        "#2c71d2, " +
+                        "derive(#2c71d2, 50%)" +
+                        ");"
+        );
+        splashLayout.setEffect(new DropShadow());
+    }
+
     @Override
     public void start(Stage stage) throws Exception{
-        // change to "view/EconoMe.fxml" when building with gradle and move EconoMe.fxml to resources/view/fxml
-        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("main/java/view/EconoMe.fxml"));
+        final Task<Parent> loaded = new Task<Parent>(){
+            @Override
+            protected Parent call() throws Exception {
+                // change to "view/EconoMe.fxml" when building with gradle and move EconoMe.fxml to resources/view/fxml
+                return FXMLLoader.load(getClass().getClassLoader().getResource("main/java/view/EconoMe.fxml"));
+            }
+        };
 
-        Scene scene = new Scene(root);
+        showSplash(stage, loaded, () -> showMainStage(loaded.valueProperty()));
+        new Thread(loaded).start();
+    }
+
+    private void showSplash(Stage initStage, Task<?> task, InitCompletionHandler initCompletionHandler) {
+        loadProgress.progressProperty().bind(task.progressProperty());
+        task.stateProperty().addListener((observableValue, oldState, newState) -> {
+            if (newState == Worker.State.SUCCEEDED){
+                loadProgress.progressProperty().unbind();
+                loadProgress.setProgress(1);
+                initStage.toFront();
+                FadeTransition fadeSplash = new FadeTransition(Duration.seconds(1.2), splashLayout);
+                fadeSplash.setFromValue(1.0);
+                fadeSplash.setToValue(0.0);
+                fadeSplash.setOnFinished(actionEvent -> initStage.hide());
+                fadeSplash.play();
+                initCompletionHandler.complete();
+            }
+        });
+
+        Scene splashScene = new Scene(splashLayout, Color.TRANSPARENT);
+        final Rectangle2D bounds = Screen.getPrimary().getBounds();
+        initStage.setScene(splashScene);
+        initStage.setX(bounds.getMinX() + bounds.getWidth() / 2 - SPLASH_WIDTH / 2);
+        initStage.setY(bounds.getMinY() + bounds.getHeight() / 2 - SPLASH_HEIGHT / 2);
+        initStage.initStyle(StageStyle.TRANSPARENT);
+        initStage.setAlwaysOnTop(true);
+        // change to "logo.png" when building with gradle
+        initStage.getIcons().add(new Image(getClass().getClassLoader().getResourceAsStream("main/resources/logo.png")));
+        initStage.show();
+    }
+
+    private void showMainStage(ReadOnlyObjectProperty<Parent> root) {
+        Scene scene = new Scene(root.getValue());
+        Stage stage = new Stage();
         // change to "logo.png" when building with gradle
         stage.getIcons().add(new Image(getClass().getClassLoader().getResourceAsStream("main/resources/logo.png")));
         stage.setTitle("EconoMe");
@@ -21,6 +101,9 @@ public class MainApp extends Application{
         stage.show();
     }
 
+    public interface InitCompletionHandler {
+        void complete();
+    }
     public static void main(String[] args) {
         launch(args);
     }
